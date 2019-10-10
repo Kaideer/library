52e1b51368f31f4f63c4cc4df8a18df57abda767
Kaideer <2024468244@qq.com>
2019-10-10 16:01:10 +0800

    修改EventBus，如果需要使用EventBus就重写isRegisterEventBus方法返回true即可

--- app/src/main/java/com/kaider/mymvplibrary/MainActivity.java
+++ app/src/main/java/com/kaider/mymvplibrary/MainActivity.java
@@ -4,34 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.kaider.library.base.BaseActivity;
-import com.kaider.library.toastutils.ToastUtil;
-
-public class MainActivity extends BaseActivity {
-
+public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected int initLayout() {
-        return R.layout.activity_main;
-    }
-
-    @Override
-    public boolean isRegisterEventBus() {
-        return false;
-    }
-
-    @Override
-    protected void initView() {
-        ToastUtil.showLong(context, "测试library");
-    }
-
-    @Override
-    protected void preparingData() {
-
-    }
-
-    @Override
-    protected void destroy() {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
     }
 }
